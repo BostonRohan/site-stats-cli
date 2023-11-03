15635c6a0d77553046b0cc1896603f6ce2e01f95
BostonRohan <bostonrohan@gmail.com>
2023-11-03 17:33:02 -0400

    grab input and parse it as http url

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Write};
+use url::Url;
+
+fn main() -> io::Result<()> {
+    println!("Welcome to site stats!");
+
+    let mut input = String::new();
+
+
+    print!("Enter your site url:");
+
+    io::stdout().flush().expect("There was an error writing to the input");
+
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+            println!("You entered: {}", input);
+            input = input.trim().to_string();
+
+            if Url::parse(&input).is_ok() {
+                println!("valid url");
+            } else {
+                println!("invalid url");
+            }
+            Ok(())
+        }
+        Err(error) => {
+            eprintln!("Error reading input: {}", error);
+            Err(error)
+        }
+    }
 }
