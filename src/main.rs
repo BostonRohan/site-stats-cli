05055f752daa7eb0bd87c164e6ff9aa7f5547a25
BostonRohan <bostonrohan@gmail.com>
2024-01-06 17:42:43 -0500

    add / to the end of url if it isnt there & split up robots.txt logic

--- src/main.rs
+++ src/main.rs
@@ -16,18 +16,22 @@ async fn process_url(input: &str) -> Result<(), Box<dyn Error>> {
         return Err("The site that was provided either does not exist or cannot be reached at the moment, please try again.".into());
     }
 
-    //ping the robots.txt file and see if it exists on the following site
-    if !reqwest::get(format!("{}{}", url, "robots.txt"))
+    Ok(())
+}
+
+async fn process_robots_txt(input: &str) -> Result<(), Box<dyn Error>>{
+        //ping the robots.txt file and see if it exists on the following site
+        if !reqwest::get(format!("{}{}", input, "robots.txt"))
         .await?
         .status()
         .is_success()
     {
         println!("The following site does not have a robots.txt file, if this is your site - it is advised that you create one. \nhttps://developers.google.com/search/docs/crawling-indexing/robots/intro#:~:text=A%20robots.txt%20file%20tells,or%20password%2Dprotect%20the%20page. ");
     }
-
-    //TODO: read that robots txt file to find a sitemap(s)
+            //TODO: read that robots txt file to find a sitemap(s)
 
     Ok(())
+
 }
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -45,9 +49,17 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .read_line(&mut input)
         .expect("There was an error reading the input");
 
+
     input = input.trim().to_string();
 
+    //add a / to the end of the url if it isn't there
+    if input.chars().last().unwrap() != '/' {
+        input = input + "/";
+    }
+
     process_url(&input).await?;
+    process_robots_txt(&input).await?;
+
 
     Ok(())
 }
